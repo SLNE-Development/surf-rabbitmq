b00ec0708efc72016b945a1c25b5f97400b57ca8
twisti <76837088+twisti-dev@users.noreply.github.com>
2026-05-25 16:44:43 +0200

    [ci skip] 🔧 chore(build): add slneReleases repository for publishing
    - include slneReleases() in publishing block of build.gradle.kts

--- surf-rabbitmq-ksp/build.gradle.kts
+++ surf-rabbitmq-ksp/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.google.devtools.ksp.gradle.KSP_VERSION
+import dev.slne.surf.api.gradle.util.slneReleases
 
 plugins {
     id("dev.slne.surf.api.gradle.core")
@@ -10,4 +11,10 @@ dependencies {
     val kotlinPoetVersion = "2.3.0"
     implementation("com.squareup:kotlinpoet-jvm:$kotlinPoetVersion")
     implementation("com.squareup:kotlinpoet-ksp:$kotlinPoetVersion")
+}
+
+publishing {
+    repositories {
+        slneReleases()
+    }
 }
